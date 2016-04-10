b52064978e1a04fd15eb9cf9f5a9dbc2bdba42ed
Renaud Hager <rhager@rhager-mbp.home>
2016-04-10 22:57:16 +0100

    Fix mode on folder www.

--- site/profiles/manifests/aptlyproxy.pp
+++ site/profiles/manifests/aptlyproxy.pp
@@ -4,14 +4,14 @@ class profiles::aptlyproxy (){
     ensure => 'directory',
     owner  => 'root',
     group  => 'root',
-    mode   => '744',
+    mode   => '755',
   }
 
   file { '/etc/nginx/www/27B9DA79.asc':
     ensure  => 'present',
     owner   => 'root',
     group   => 'root',
-    mode    => '744',
+    mode    => '644',
     source  => 'puppet:///modules/profiles/aptlyproxy/aptly.pub',
   }
 }
