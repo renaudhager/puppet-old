60fa7a205aaa991aef528c3f30566778cf3dd1e5
Renaud Hager <rhager@rhager-mbp.home>
2016-04-10 21:02:32 +0100

    Add default hash.

--- site/profiles/manifests/aptlymirrors.pp
+++ site/profiles/manifests/aptlymirrors.pp
@@ -1,5 +1,11 @@
 class profiles::aptlymirrors (){
   $mirrors         = hiera_hash( 'profiles::aptlymirrors', {} )
+
   validate_hash( $mirrors )
+
+  $defaults = {
+      ensure => present,
+  }
+
   create_resources( aptly::mirror, $mirror, $defaults )
 }
