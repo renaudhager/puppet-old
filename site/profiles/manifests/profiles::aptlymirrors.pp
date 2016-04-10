631c584047507ae6d3812b3db69002520498b742
Renaud Hager <rhager@rhager-mbp.home>
2016-04-10 20:57:37 +0100

    Create a wrapper for aptly mirror.

---
+++ site/profiles/manifests/profiles::aptlymirrors.pp
@@ -0,0 +1,5 @@
+class profiles::aptlymirrors (){
+  $mirrors         = hiera_hash( 'profiles::aptlymirrors', {} )
+  validate_hash( $mirrors )
+  create_resources( aptly::mirror, $mirror, $defaults )
+}
